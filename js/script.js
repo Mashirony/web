f087e2c2ae817e4dad040124e550e1ba5bc34529
Dima <cruselnitschiid@mail.ru>
2020-08-09 23:06:39 +0300

    web

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+    var link = $('.m-menu-link');
+    var menu = $('.m-menu');  
+    var close = $('.close-menu')
+ 
+ link.on('click', function(event) {
+    event.preventDefault();
+    menu.toggleClass('m-menu__active');
+ });
+ close.on('click', function(event) {
+    event.preventDefault();
+    menu.toggleClass('m-menu__active');
+ });
+ });
